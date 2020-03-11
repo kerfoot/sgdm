270a848888fccbeeafa305409c6380eac004c22f
John Kerfoot <johnkerfoot@gmail.com>
2020-03-10 20:59:08 -0400

    added default encodings

--- sgdm/constants.py
+++ sgdm/constants.py
@@ -3,3 +3,7 @@ dba_data_types = {'1': 'i1',
                   '4': 'f4',
                   '8': 'f8'}
 
+default_encoding = {'zlib': True,
+                    'complevel': 1,
+                    'dtype': 'f8'}
+
